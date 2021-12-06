7632957ac571ec08d0b8c7ab029362d4b4d3651b
Anapom <prachaya.udom@gmail.com>
2021-12-06 16:04:55 +0700

    first commit

--- iot/main.py
+++ iot/main.py
@@ -1,13 +1,50 @@
 import base64
 import json
 import os
-from google.cloud import iot_v1
+from math import cos, sin, radians, sqrt
+from google.cloud import firestore
 
+
+client = firestore.Client(project='bigdata-anapom')
 
 def subscribe(event, context):
-    data = base64.b64decode(event['data'])
-    print(f'data in function {data}')
-    client = iot_v1.DeviceManagerClient()
-    device_path = client.device_path("halogen-byte-329812","us-central1","temperature-registry","temperature-fan-device")
-    if int(data) > 50:
-        client.send_command_to_device(request={"name":device_path,"binary_data":data})
+    message = base64.b64decode(event['data']).decode('utf-8')
+    print(f'data in function {message}')
+    data = json.loads((message))
+    node_id = int(data['node_id'])
+    voltageA = float(data['voltageA'])
+    voltageB = float(data['voltageB'])
+    voltageC = float(data['voltageC'])
+    print(f'voltage {voltageA} , {voltageB}, {voltageC}')
+  
+    # calculate unbalance voltage based on IEC
+    PA = 0.0
+    PB = 240.0
+    PC = 120.0
+    top_left  = voltageA * cos(radians(PA)) + voltageB * cos(radians(PB+240)) + voltageC * cos(radians(PC+120))
+    top_right = voltageA * sin(radians(PA)) + voltageB * sin(radians(PB+240)) + voltageC * sin(radians(PC+120))
+    top = sqrt((top_left * top_left) + (top_right *top_right))
+
+    bot_left  = voltageA * cos(radians(PA)) + voltageB * cos(radians(PB+120)) + voltageC * cos(radians(PC+240))
+    bot_right = voltageA * sin(radians(PA)) + voltageB * sin(radians(PB+120)) + voltageC * sin(radians(PC+240))
+    bot = sqrt((bot_left * bot_left) + (bot_right *bot_right))
+
+    unbalance = top / bot * 100
+
+    if unbalance > 40:
+        ret = "unbalance_voltage"
+    else:
+        ret = "normal_data"
+
+    print(f'{node_id} status : {ret}')
+
+    # save to firestore
+    documentName = 'transformer' + str(node_id)
+    doc = client.collection('trasnformerData').document(documentName)
+    doc.set({
+            'transformer_id': node_id,
+            'status' : ret,
+            'VoltageA' : voltageA,
+            'VoltageB' : voltageB,
+            'VoltageC' : voltageC
+        })
--- iot/mqtt_producer_demo.py
+++ iot/mqtt_producer_demo.py
@@ -2,6 +2,7 @@ import argparse
 import datetime
 import ssl
 import time
+import json
 import jwt
 import paho.mqtt.client as mqtt
 
@@ -54,14 +55,21 @@ parser.add_argument("--project_id", required=True)
 parser.add_argument("--cloud_region", required=True)
 parser.add_argument("--registry_id", required=True)
 parser.add_argument("--device_id", required=True)
-parser.add_argument("--temperature", required=True)
+parser.add_argument("--voltageA", required=True)
+parser.add_argument("--voltageB", required=True)
+parser.add_argument("--voltageC", required=True)
+parser.add_argument("--node_id", required=True)
 args = parser.parse_args()
 
 project_id = args.project_id
 cloud_region = args.cloud_region
 registry_id = args.registry_id
 device_id = args.device_id
-payload = args.temperature
+node_id = args.node_id
+voltageA = args.voltageA
+voltageB = args.voltageB
+voltageC = args.voltageC
+payload = json.dumps({"node_id":node_id, "voltageA":voltageA, "voltageB":voltageB, "voltageC":voltageC})
 
 client_id = "projects/{}/locations/{}/registries/{}/devices/{}".format(
         project_id, cloud_region, registry_id, device_id
